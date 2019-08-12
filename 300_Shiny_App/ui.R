381aa29bb8b414c9b37dc6a78688a8e83f83e76f
Edmund Tse <etse@microsoft.com>
2019-08-11 23:43:28 -0700

    Add Shiny app skeleton layout

---
+++ 300_Shiny_App/ui.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(shinyWidgets)
+
+shinyUI(
+    fluidPage(
+        theme = shinythemes::shinytheme('darkly'),
+
+        shinyWidgets::useShinydashboard(),
+
+        titlePanel('Movie Moods'),
+
+        sidebarLayout(
+            sidebarPanel(
+                selectInput('filter1', 'Filter 1', c('Choice')),
+                sliderInput(
+                    'filter2',
+                    'Filter 2',
+                    min = 1,
+                    max = 50,
+                    value = 30
+                ),
+                sliderInput(
+                    'filter3',
+                    'Filter 3',
+                    min = 1,
+                    max = 50,
+                    value = 30
+                ),
+                sliderInput(
+                    'filter4',
+                    'Filter 4',
+                    min = 1,
+                    max = 50,
+                    value = 30
+                )
+            ),
+
+            mainPanel(
+                fluidRow(
+                    box(
+                        title = "Movies",
+                        width = 6,
+                        height = 300
+                    ),
+                    box(
+                        title = "Mood star for selected movies",
+                        width = 6,
+                        height = 300
+                    )
+                ),
+                fluidRow(
+                    box(
+                        title = "Characters",
+                        width = 6,
+                        height = 300
+                    ),
+                    box(
+                        title = "Mood star for selected characters",
+                        width = 6,
+                        height = 300
+                    )
+                ),
+                fluidRow(
+                    box(
+                        title = "Character mood progression",
+                        width = 12,
+                        height = 300
+                    )
+                )
+            )
+        )
+    )
+)
