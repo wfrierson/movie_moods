acadeb1bc49ac51a4281c69e4abe13755ac7f9e4
Edmund Tse <edmund@outlook.com>
2019-08-18 12:14:53 -0700

    Move dataset into a shared global.R

---
+++ 300_Shiny_App/global.R
@@ -0,0 +1,9 @@
+# Prepare a dummy dataset
+dataset <- mtcars %>%
+  tibble::rownames_to_column() %>%
+  dplyr::transmute(
+    Movie = rowname,
+    Genre = cyl,
+    x = wt - mean(wt),
+    y = mpg - mean(mpg)
+  )
--- 300_Shiny_App/server.R
+++ 300_Shiny_App/server.R
@@ -2,16 +2,6 @@ library(shiny)
 library(tidyverse)
 
 shinyServer(function(input, output) {
-  # Prepare a dummy dataset
-  dataset <- mtcars %>%
-    tibble::rownames_to_column() %>%
-    dplyr::transmute(
-      Movie = rowname,
-      Genre = cyl,
-      x = wt - mean(wt),
-      y = mpg - mean(mpg)
-    )
-
   # Start the server for the movieMoodLandscape module
   movieMoodLandscape <- shiny::callModule(
     moodLandscapeServer,
