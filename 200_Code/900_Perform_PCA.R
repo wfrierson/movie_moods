e39c788283a22ce637e3f5f1830bbf1f2a9cc170
Will Frierson <will.frierson@gmail.com>
2019-08-19 16:21:12 -0700

    master: Adding 900_Perform_PCA script which returns aggregated mood tables rotated as PCs.

---
+++ 200_Code/900_Perform_PCA.R
@@ -0,0 +1,133 @@
+###############################################################################
+# This script performs principal component analysis on the movie-level
+# aggregations of moods.
+
+###############################################################################
+# SETUP
+
+folder.data <- '100_Data'
+folder.data.processed <- file.path(folder.data, '120_Processed_Data')
+folder.data.raw <- file.path(folder.data, '110_Raw_Data')
+folder.code <- '200_Code'
+
+path.dependencies <- file.path(folder.code, '000_Dependencies.R')
+path.pcaFunctions <- file.path(folder.code, '800_PCA_Functions.R')
+source(path.dependencies)
+source(path.pcaFunctions)
+
+###############################################################################
+# IMPORT DATA
+
+screenplayPaths <- data.table::fread(
+  file.path(folder.data.processed, '301_screenplayPaths.csv'),
+  stringsAsFactors = FALSE,
+  quote = ""
+)
+
+screenplayMoodProb.movie <- data.table::fread(
+  file.path(folder.data.processed, '702_screenplayMoodProb.movie.csv'),
+  stringsAsFactors = FALSE,
+  quote = "",
+  sep = '|',
+  key = c('movie')
+)
+
+screenplayMoodProb.character <- data.table::fread(
+  file.path(folder.data.processed, '703_screenplayMoodProb.character.csv'),
+  stringsAsFactors = FALSE,
+  quote = "",
+  sep = '|',
+  key = c('movie', 'character')
+)
+
+screenplayMoodProb.scene <- data.table::fread(
+  file.path(folder.data.processed, '704_screenplayMoodProb.scene.csv'),
+  stringsAsFactors = FALSE,
+  quote = "",
+  sep = '|',
+  key = c('movie', 'sceneNumber')
+)
+
+screenplayMoodProb.sceneCharacter <- data.table::fread(
+  file.path(folder.data.processed, '705_screenplayMoodProb.sceneCharacter.csv'),
+  stringsAsFactors = FALSE,
+  quote = "",
+  sep = '|',
+  key = c('movie', 'sceneNumber', 'character')
+)
+
+###############################################################################
+# PERFORM PCA ON MOVIE-LEVEL MOOD AGGREGATIONS
+
+# Create vector of DM++ moods and column names
+moods.DMpp <- c(
+  'afraid', 'amused', 'angry', 'annoyed', 'dont_care', 'happy', 'inspired',
+  'sad'
+)
+
+# Perform PCA at movie-level
+pcaMovie <- prcomp(
+  screenplayMoodProb.movie[, mget(moods.DMpp)],
+  center = TRUE,
+  scale. = FALSE,
+  retx = TRUE
+)
+
+# Get vector of genre labels to append to each movie
+genres <- colnames(screenplayPaths)[-c(1:2, 25)] %>% copy
+
+# Rotate each mood aggregation table for visualizing downstream
+screenplayMoodProb.aggregationsRot <- lapply(
+  c(
+    'screenplayMoodProb.movie', 'screenplayMoodProb.character', 
+    'screenplayMoodProb.scene','screenplayMoodProb.sceneCharacter'
+  ),
+  rotateMoodAggregation
+)
+
+###############################################################################
+# EXPORT RESULTS
+
+data.table::fwrite(
+  screenplayMoodProb.aggregationsRot[[1]],
+  file = file.path(
+            folder.data.processed,
+            '901_screenplayMoodProb.movieRotated.csv'
+         ),
+  row.names = FALSE,
+  quote = FALSE,
+  sep = '|'
+)
+
+data.table::fwrite(
+  screenplayMoodProb.aggregationsRot[[2]],
+  file = file.path(
+            folder.data.processed,
+            '902_screenplayMoodProb.characterRotated.csv'
+         ),
+  row.names = FALSE,
+  quote = FALSE,
+  sep = '|'
+)
+
+data.table::fwrite(
+  screenplayMoodProb.aggregationsRot[[3]],
+  file = file.path(
+            folder.data.processed,
+            '903_screenplayMoodProb.sceneRotated.csv'
+         ),
+  row.names = FALSE,
+  quote = FALSE,
+  sep = '|'
+)
+
+data.table::fwrite(
+  screenplayMoodProb.aggregationsRot[[4]],
+  file = file.path(
+            folder.data.processed,
+            '904_screenplayMoodProb.sceneCharacterRotated.csv'
+         ),
+  row.names = FALSE,
+  quote = FALSE,
+  sep = '|'
+)
