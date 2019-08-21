c755aebc4a0abd9659efc2f6d933e63851779a0c
Will Frierson <will.frierson@gmail.com>
2019-08-21 00:54:00 -0700

    connect_movies_to_characters: Failed attempt to connect movie search output to filter character data.

--- 300_Shiny_App/MoodLandscape.R
+++ 300_Shiny_App/MoodLandscape.R
@@ -50,7 +50,7 @@ moodLandscapeIxDebugUi <- function(id) {
 #' Mood Landscape plot module server-side processing
 #'
 #' @param input, output, session standard \code{shiny} boilerplate
-#' @param dataset data frame (reactive)
+#' @param dataset data frame (non-reactive) with variables \code{x} and \code {y}
 #' @param searchHighlightCol (non-reactive) variable for the name of column to
 #' use when filtering for highlight in the visual
 moodLandscapeServer <- function(input,
@@ -60,7 +60,8 @@ moodLandscapeServer <- function(input,
                                 xCol,
                                 yCol,
                                 searchHighlightCol,
-                                text) {
+                                text,
+                                searchResultName) {
   ns = session$ns
   
   # Dynamically render the selectizeInput UI
@@ -68,7 +69,7 @@ moodLandscapeServer <- function(input,
     selectizeInput(
       ns("search"),
       "Search by Title",
-      choices = c("Select up to 5" = "", dataset()[[searchHighlightCol]]),
+      choices = c("Select up to 5" = "", dataset[[searchHighlightCol]]),
       multiple = TRUE,
       options = list(maxItems = 5)
     )
@@ -82,7 +83,7 @@ moodLandscapeServer <- function(input,
 
   plot_obj <- shiny::reactive({
     p <- plotly::plot_ly(
-      data = dataset(),
+      data = dataset,
       x = ~get(xCol),
       y = ~get(yCol),
       type = "scatter",
@@ -119,7 +120,7 @@ moodLandscapeServer <- function(input,
   # Return reactiveValues for downstream use
   vals <- reactiveValues()
   observe({
-    
+    vals[[searchResultName]] <- input$search
   })
 
   return(vals)
