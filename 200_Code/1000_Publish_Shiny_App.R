16120a7b39b46cb7cf65057924ea0edabf3c00a1
Edmund Tse <edmund@outlook.com>
2019-08-24 14:50:18 -0700

    Code to package and publish to ShinyApps.io

---
+++ 200_Code/1000_Publish_Shiny_App.R
@@ -0,0 +1,57 @@
+###############################################################################
+# This script publishes the Shiny app to ShinyApps.io
+
+###############################################################################
+# DEPENDENCIES
+
+library(rsconnect)
+
+###############################################################################
+# SETUP
+
+folder.data <- "100_Data"
+folder.data.processed <- file.path(folder.data, "120_Processed_Data")
+folder.shinyapp <- "300_Shiny_App"
+folder.shinyrelease <- "release"
+
+###############################################################################
+# ASSEMBLE APP FOLDER
+
+# Create the release folder
+if (!dir.exists(folder.shinyrelease)) {
+  dir.create(folder.shinyrelease)
+}
+
+# Copy code files
+app.files <- list.files(folder.shinyapp, full.names = TRUE, recursive = TRUE)
+file.copy(app.files, folder.shinyrelease, overwrite = TRUE)
+
+# Fix the folder paths
+filesToFix <- file.path(folder.shinyrelease, c("server.R", "ui.R"))
+sapply(filesToFix, function(filename) {
+  txt <- gsub(
+    pattern = '^folder.data.processed <- .*$',
+    replace = 'folder.data.processed <- (".")',
+    x = readLines(filename))
+  writeLines(txt, con = filename)
+})
+
+# Copy data files
+data.files <- c(
+  "301_screenplayPaths.csv",
+  "702_screenplayMoodProb.movie.csv",
+  "703_screenplayMoodProb.character.csv",
+  "901_screenplayMoodProb.movieRotated.csv",
+  "902_screenplayMoodProb.characterRotated.csv"
+)
+file.copy(file.path(folder.data.processed, data.files), folder.shinyrelease)
+
+###############################################################################
+# TEST THE APP PACKAGE
+
+runApp(folder.shinyrelease)
+
+###############################################################################
+# PUBLISH TO SHINYAPPS.IO
+
+rsconnect::deployApp(folder.shinyrelease, appName = "MovieMoods")
